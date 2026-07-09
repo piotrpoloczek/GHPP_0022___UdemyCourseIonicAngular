50216feb0348999d7ae241161f40195d23e4200a
Piotr Poloczek <108583263+piotrpoloczek@users.noreply.github.com>
2026-07-09 21:15:50 +0200

    add the project pages

---
+++ ionic-angular-course/src/app/places/offer/offer-routing.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { OfferPage } from './offer.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: OfferPage
+  },
+  {
+    path: 'new-offer',
+    loadChildren: () => import('./new-offer/new-offer.module').then( m => m.NewOfferPageModule)
+  },
+  {
+    path: 'edit',
+    loadChildren: () => import('./edit/edit.module').then( m => m.EditPageModule)
+  },
+  {
+    path: 'edit-offer',
+    loadChildren: () => import('./edit-offer/edit-offer.module').then( m => m.EditOfferPageModule)
+  },
+  {
+    path: 'place-bookings',
+    loadChildren: () => import('./place-bookings/place-bookings.module').then( m => m.PlaceBookingsPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class OfferPageRoutingModule {}
