84e38bcb65197aaf8261871c97918cfc22c2c5b0
Piotr Poloczek <108583263+piotrpoloczek@users.noreply.github.com>
2026-07-17 21:05:50 +0200

    Finish handling user input

--- ionic-angular-course/src/app/bookings/create-booking/create-booking.component.ts
+++ ionic-angular-course/src/app/bookings/create-booking/create-booking.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { ModalController } from '@ionic/angular';
 import { Place } from 'src/app/places/place.model';
 
@@ -9,17 +10,57 @@ import { Place } from 'src/app/places/place.model';
 })
 export class CreateBookingComponent implements OnInit {
   @Input() selectedPlace: Place | undefined;
+  @Input() selectedMode: 'select' | 'random' | undefined;
+  @ViewChild('f', { static: true}) form: NgForm | undefined;
+  startDate: string | undefined;
+  endDate: string | undefined;
 
   constructor(private modalCtrl: ModalController) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const availableFrom = new Date(this.selectedPlace!.availableFrom);
+    const availableTo = new Date(this.selectedPlace!.availableTo);
+
+    if (this.selectedMode === 'random') {
+      this.startDate = new Date(
+        availableFrom.getTime() +
+          Math.random() *
+            (availableTo.getTime() -
+              7 * 24 * 60 * 60 * 1000 -
+              availableFrom.getTime())
+      ).toISOString();
+
+      this.endDate = new Date(
+        new Date(this.startDate).getTime() +
+          Math.random() * (new Date(this.startDate).getTime() + 6 * 24 * 60 * 60 * 1000 - new Date(this.startDate).getTime())
+      ).toISOString();
+    }
+  }
 
   onBookPlace() {
-    this.modalCtrl.dismiss({ message: 'This is a dummy message!' }, 'confirm');
+    if (!this.form?.valid || !this.datesValid()) {
+      return;
+    }
+
+    this.modalCtrl.dismiss({
+      bookingData: {
+        firstName: this.form.value.firstName,
+        lastName: this.form.value.lastName,
+        guestNumber: +this.form.value.guestNumber,
+        dateFrom: new Date(this.form.value.dateFrom),
+        dateTo: new Date(this.form.value.dateTo)
+      }
+    }, 'confirm');
   }
 
   onCancel() {
     this.modalCtrl.dismiss(null, 'cancel');
+  }
+
+  datesValid() {
+    const startDate = new Date(this.form!.value.dateFrom);
+    const endDate = new Date(this.form!.value.dateTo);
+    return endDate > startDate;
   }
 
 }
