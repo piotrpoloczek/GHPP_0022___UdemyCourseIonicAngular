22d89cf0969c8b2e227c5da0961e8bc01a1c5dda
Piotr Poloczek <108583263+piotrpoloczek@users.noreply.github.com>
2026-07-26 19:48:47 +0200

    Using native device features

--- ionic-angular-course/src/app/shared/shared.module.ts
+++ ionic-angular-course/src/app/shared/shared.module.ts
@@ -3,10 +3,11 @@ import { MapModalComponent } from "./map-modal/map-modal.component";
 import { LocationPickerComponent } from "./pickers/location-picker/location-picker.component";
 import { CommonModule } from "@angular/common";
 import { IonicModule } from "@ionic/angular";
+import { ImagePickerComponent } from "./pickers/image-picker/image-picker.component";
 
 @NgModule({
-    declarations: [LocationPickerComponent, MapModalComponent],
-    exports: [LocationPickerComponent, MapModalComponent],
+    declarations: [LocationPickerComponent, MapModalComponent, ImagePickerComponent],
+    exports: [LocationPickerComponent, MapModalComponent, ImagePickerComponent],
     imports: [CommonModule, IonicModule],
     entryComponents: [MapModalComponent],
     providers: []
