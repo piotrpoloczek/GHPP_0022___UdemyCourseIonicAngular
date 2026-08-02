50a708f1ab1f1d4330e8661e92731b3bd19cbeef
Piotr Poloczek <108583263+piotrpoloczek@users.noreply.github.com>
2026-08-02 21:23:44 +0200

    Adding autologout

--- ionic-angular-course/src/app/auth/auth.service.ts
+++ ionic-angular-course/src/app/auth/auth.service.ts
@@ -20,8 +20,9 @@ export interface AuthResponseData {
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthService implements OnDestroy {
   private _user = new BehaviorSubject<User>(null);
+  private activeLogoutTimer: any;
 
   get userIsAuthenticated() {
     return this._user.asObservable().pipe(
@@ -75,6 +76,7 @@ export class AuthService {
       tap(user => {
         if (user) {
           this._user.next(user);
+          this.autoLogout(user.tokenDuration);
         }
       }),
       map(user => {
@@ -110,18 +112,33 @@ export class AuthService {
     Plugins.Storage.remove({key: 'authData'});
   }
 
+  ngOnDestroy() {
+    if (this.activeLogoutTimer) {
+      clearTimeout(this.activeLogoutTimer);
+    }
+  }
+
+  private autoLogout(duration: number) {
+    if (this.activeLogoutTimer) {
+      clearTimeout(this.activeLogoutTimer);
+    }
+    this.activeLogoutTimer = setTimeout(() => {
+      this.logout();
+    }, duration);
+  }
+
   private setUserData(userData: AuthResponseData) {
     const expirationTime = new Date(
       new Date().getTime() + +userData.expiresIn * 1000
     );
-    this._user.next(
-      new User(
+    const user = new User(
         userData.localId,
         userData.email,
         userData.idToken,
         expirationTime
       )
-    );
+    this._user.next(user);
+    this.autoLogout(user.tokenDuration);
     this.storeAuthData(
       userData.localId,
       userData.idToken,
