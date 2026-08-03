cdb1ce7b3151781a6c6b624048a1218caa6f5ac5
Piotr Poloczek <108583263+piotrpoloczek@users.noreply.github.com>
2026-08-03 19:40:20 +0200

    Requiring the auth token on the backend

--- ionic-angular-course/functions/index.js
+++ ionic-angular-course/functions/index.js
@@ -1,32 +1,94 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true });
+const Busboy = require('busboy');
+const os = require('os');
+const path = require('path');
+const fs = require('fs');
+const uuid = require('uuid/v4');
+const fbAdmin = require('firebase-admin');
 
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
+const { Storage } = require('@google-cloud/storage');
 
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+const storage = new Storage({
+  projectId: 'YOUR_FIREBASE_PROJECT_ID'
+});
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+fbAdmin.initializeApp({
+  credential: fbAdmin.credential.cert(require('YOUR_FIREBASE_CREDENTIALS'))
+});
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.storeImage = functions.https.onRequest((req, res) => {
+  return cors(req, res, () => {
+    if (req.method !== 'POST') {
+      return res.status(500).json({ message: 'Not allowed.' });
+    }
+
+    if (
+      !req.headers.authorization ||
+      !req.headers.authorization.startsWith('Bearer ')
+    ) {
+      return res.status(401).json({ error: 'Unauthorized!' });
+    }
+
+    let idToken;
+    idToken = req.headers.authorization.split('Bearer ')[1];
+
+    const busboy = new Busboy({ headers: req.headers });
+    let uploadData;
+    let oldImagePath;
+
+    busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
+      const filePath = path.join(os.tmpdir(), filename);
+      uploadData = { filePath: filePath, type: mimetype, name: filename };
+      file.pipe(fs.createWriteStream(filePath));
+    });
+
+    busboy.on('field', (fieldname, value) => {
+      oldImagePath = decodeURIComponent(value);
+    });
+
+    busboy.on('finish', () => {
+      const id = uuid();
+      let imagePath = 'images/' + id + '-' + uploadData.name;
+      if (oldImagePath) {
+        imagePath = oldImagePath;
+      }
+
+      return fbAdmin
+        .auth()
+        .verifyIdToken(idToken)
+        .then(decodedToken => {
+          console.log(uploadData.type);
+          return storage
+            .bucket('YOUR_FIREBASE_PROJECT_ID.appspot.com')
+            .upload(uploadData.filePath, {
+              uploadType: 'media',
+              destination: imagePath,
+              metadata: {
+                metadata: {
+                  contentType: uploadData.type,
+                  firebaseStorageDownloadTokens: id
+                }
+              }
+            });
+        })
+        .then(() => {
+          return res.status(201).json({
+            imageUrl:
+              'https://firebasestorage.googleapis.com/v0/b/' +
+              storage.bucket('YOUR_FIREBASE_PROJECT_ID.appspot.com').name +
+              '/o/' +
+              encodeURIComponent(imagePath) +
+              '?alt=media&token=' +
+              id,
+            imagePath: imagePath
+          });
+        })
+        .catch(error => {
+          console.log(error);
+          return res.status(401).json({ error: 'Unauthorized!' });
+        });
+    });
+    return busboy.end(req.rawBody);
+  });
+});
